a474e798df9563b3baa030d0c960fad1a93c93c7
Rafael Coutinho <coutinhor@knowfully.com>
2023-09-25 11:33:21 -0300

    added recipe service

--- src/app/recipes/recipe.services.ts
+++ src/app/recipes/recipe.services.ts
@@ -1,12 +1,21 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe 1', 'hfjdsk fhdjsk fhdjsk fhdsjk', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'),
-    new Recipe('Test Recipe 2', 'bla bla bla', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'),
+    new Recipe('Test Recipe 1', 'hfjdsk fhdjsk fhdjsk fhdsjk', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bread', 1),
+      ]),
+    new Recipe('Test Recipe 2', 'bla bla bla', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bread', 1),
+      ]),
   ];
 
   getRecipes() {
