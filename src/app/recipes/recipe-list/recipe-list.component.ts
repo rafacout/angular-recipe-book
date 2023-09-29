7d097cb24e5deda93cef64c6bccdd6aa1e29dd29
Rafael Coutinho <coutinhor@knowfully.com>
2023-09-29 10:59:46 -0300

    changes on routes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import {RecipeService} from "../recipe.services";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,7 +11,13 @@ import {RecipeService} from "../recipe.services";
 export class RecipeListComponent {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute) {
     this.recipes = recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 }
